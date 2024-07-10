31b6284e578eaa640e997a75085448bb8bed8739
YuriiTsykvas <yurii.t@devmood.tech>
2024-07-10 16:45:17 +0300

    Custom buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,29 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 40,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 12,
+    elevation: 80, // shadow. Works only with Android
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2}, // shadow offset for IOS
+    shadowRadius: 6, //  shadow radius for IOS
+    shadowOpacity: '0,25' // shadow opacity for IOS
+  }
+});
 
 export default StartGameScreen;
