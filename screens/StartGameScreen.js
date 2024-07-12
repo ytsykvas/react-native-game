436b03afe16d60e5b307b07def9860192a8c0f16
YuriiTsykvas <yurii.t@devmood.tech>
2024-07-12 16:03:53 +0300

    Started with main logic

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,50 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number should be between 1 and 99',
+        [{text: 'OK', style: 'destructive', onPress: resetInputHandler}]
+      );
+      return;
+    }
+
+    console.log('Valid number!');r
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
+        onChange={numberInputHandler}
+        value={enteredNumber}
         // autoCapitalize="none" // works with none numeric symbols. Disabling capitalize. Do not needed in this case.
-        // autoCorrect={false} // works with none numeric symbols. Disabling autocorrection. Do not needed in this case.
+        autoCorrect={false} // works with none numeric symbols. Disabling autocorrection. Do not needed in this case.
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
